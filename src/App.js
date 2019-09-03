a41359bd962f33b3d6c87cb58fbe73dbbe85e252
Go Nakano <g.nakano555@gmail.com>
2019-09-03 09:35:57 +0900

    created face recognition components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import Recognition from "./Recognition";
 
 const App = () => {
   return (
     <div className="app">
+      <h1>Test</h1>
+      <Recognition></Recognition>
     </div>
   );
-}
+};
 
 export default App;
