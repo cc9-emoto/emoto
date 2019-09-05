0158e1b1c37e6160750553226a4c0fac8f54af73
Wilson Lau <hello@wilsonplau.com>
2019-09-05 12:06:06 +0900

    Quick commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import Login from './components/Login'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./styles/Reset.scss"
+import "./styles/App.scss"
+import Index from './pages/Index.js'
+import Dashboard from './pages/Dashboard.js'
+import Onboarding from './pages/Onboarding.js'
+import Visualization from './pages/Visualization.js'
+import Visualization2 from './pages/Visualization2.js'
 
 const App = () => {
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <Router>
+      <Route path="/" exact component={Index} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/onboarding" component={Onboarding} />
+      <Route path="/vis" component={Visualization} />
+      <Route path="/vis2" component={Visualization2} />
+    </Router>
   );
 };
 
