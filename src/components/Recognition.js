f5c5b423a333475b0ff9ce11bfdb10c8bfe88005
Wil Andrade and rina sakamaki <andradewilker+h0508g@gmail.com>
2019-09-06 14:05:42 +0900

    Fix for glitchy player

--- src/components/Recognition.js
+++ src/components/Recognition.js
@@ -3,33 +3,14 @@ import axios from "axios";
 import WebcamCapture from "./WebcamCapture";
 import "../styles/Webcam.scss";
 
-//The component which send image data to Azure to get info
 class Recognition extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      photo: "",
-      sendData: "",
-      responseFromAPI: {},
-      webCamData: "",
-      preview: ""
-    };
-  }
-  //submit to Azure API with params
-  submitData = base64 => {
-    axios
-      .post("/azure", { base64, userID: "abcde" })
-      .then(res => {
-        console.log(res.data);
-        const apiRes = res.data;
-        this.setState({ responseFromAPI: apiRes });
-      })
-      .catch(error => error);
+  submitData = async base64 => {
+    const response = await axios.post("/azure", { base64, userID: "abcde" });
+    return response.data;
   };
 
   getCaptureImage = async webCamData => {
-    this.submitData(webCamData);
-    const feelings = this.state.responseFromAPI;
+    const feelings = await this.submitData(webCamData);
     this.props.getNewSong(feelings.happiness + 0.5 * feelings.neutral);
   };
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,98 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-
-import Recognition from '../components/Recognition';
-import Player from '../components/Player';
-import "../styles/Dashboard.scss"
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import Recognition from "../components/Recognition";
+import Player from "../components/Player";
+import "../styles/Dashboard.scss";
 
 const Dashboard = () => {
   const [token, setToken] = useState("");
 
   const [playlist, setPlaylist] = useState([]);
-  const pushToPlaylist = (spotifyURI) => {
-    setPlaylist([...playlist, spotifyURI])
-  } 
+  const pushToPlaylist = spotifyURI => {
+    setPlaylist([...playlist, spotifyURI]);
+  };
 
   const [capture, setCapture] = useState(false);
   const toggleCapture = () => {
     setCapture(!capture);
-  }
+  };
 
   useEffect(() => {
-    const user = Cookies.get('emoto-access');
+    const user = Cookies.get("emoto-access");
     setToken(user);
-    resetAdded()
-    startingTwo()
+    resetAdded();
+    startingTwo();
   }, []);
 
   const resetAdded = async () => {
-    const user = Cookies.get('emoto-id');
-    const response = await axios.post('/graphql', {query: 
-      `mutation {
+    const user = Cookies.get("emoto-id");
+    const response = await axios.post("/graphql", {
+      query: `mutation {
         resetAdded (userId: "${user}")
       }`
-    })
-    console.log(response.data.data.resetAdded)
-  }
+    });
+  };
 
   const startingTwo = async () => {
-    const user = Cookies.get('emoto-id');
-    const starting = []
-    const response = await axios.post('/graphql', { query: `
+    const user = Cookies.get("emoto-id");
+    const starting = [];
+    const response = await axios.post("/graphql", {
+      query: `
       query {
         startingTwo (userId: "${user}") {
           songId
         }
       }
-    `})
+    `
+    });
     for (let item of response.data.data.startingTwo) {
-      starting.push(`spotify:track:${item.songId}`)
+      starting.push(`spotify:track:${item.songId}`);
     }
-    setPlaylist([...playlist, ...starting])
-  }
+    setPlaylist([...playlist, ...starting]);
+  };
 
   const requestNewToken = async () => {
-    const refreshToken = Cookies.get('emoto-refresh');
-    const response = await axios.post('/spotify/reauthorize', { refreshToken });
-    Cookies.set('emoto-access', response.data)
+    const refreshToken = Cookies.get("emoto-refresh");
+    const response = await axios.post("/spotify/reauthorize", { refreshToken });
+    Cookies.set("emoto-access", response.data);
     setToken(response.data);
-  }
+  };
 
   const getNewSong = async (value = 0.5) => {
-    console.log("Getting a new song...")
-    const response = await axios.post('/graphql', { query: `
+    console.log("Getting a new song...");
+    const response = await axios.post("/graphql", {
+      query: `
       query {
         matchingSong (value: ${value}) {
         songId
         }
       }
-    `})
+    `
+    });
     console.log(response.data);
-    const newSongId = response.data.data.matchingSong.songId
+    const newSongId = response.data.data.matchingSong.songId;
     console.log(`Got a new song! ${newSongId}`);
     pushToPlaylist(`spotify:track:${newSongId}`);
-  }
+  };
 
   return (
     <div className="dashboard">
       <div className="dashboard__top">
         <Recognition capture={capture} getNewSong={getNewSong} />
-        {/* <button onClick={() => getNewSong(0.7)}>BUTTON</button> */}
-        {/* <button onClick={toggleCapture}>CAPTURE</button> */}
-
-      </div>  
+      </div>
       <div className="dashboard__bottom">
-        <Player 
-          token={token} 
-          playlist={playlist} 
-          requestNewToken={requestNewToken} 
-          toggleCapture={toggleCapture} 
+        <Player
+          token={token}
+          playlist={playlist}
+          requestNewToken={requestNewToken}
+          toggleCapture={toggleCapture}
           getNewSong={getNewSong}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
