b03b325c4dee36164414ca2dea769f6b2311540a
Wilson Lau <hello@wilsonplau.com>
2019-09-09 17:25:02 +0900

    Implemented emotion colors in Vis3

--- src/components/Visualization3.js
+++ src/components/Visualization3.js
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react';
 import anime from 'animejs';
+import colorHelper from '../helpers/colorHelper'
 import "../styles/Visualization.scss";
 
-const Visualization3 = ({ beatsData = [], playerPlaying = true }) => {
+const Visualization3 = ({ beatsData = [], playerPlaying, emotionValue }) => {
   let time = 0;
   let counter = 0;
   const beats = new Set(beatsData.map((beat) => Math.ceil(beat.start*1000/100)*100));
 
   const animate = () => {
+    const targetHex = colorHelper.getHexFromEmotion(emotionValue);
     anime({
       targets: '.rectangle',
       height: function(e, i, l) {
         return (Math.sin(i + counter) + 2) * 25
-      }
+      },
+      fill: function() { return `#${targetHex}` }
     });
     counter++;
   }
@@ -36,7 +39,7 @@ const Visualization3 = ({ beatsData = [], playerPlaying = true }) => {
   const renderRectangles = () => {
     const array = [];
     for (let i = 0; i < 100; i++ ) {
-      array.push(<rect key={i} className="rectangle" x={i} height={(Math.sin(i) + 2) * 25} width={0.5} style={{fill: "#000"}} />);
+      array.push(<rect key={i} className="rectangle" x={i} height={(Math.sin(i) + 2) * 25} width={0.5} fill="url(#gradient)" />);
     }
     return array;
   }
