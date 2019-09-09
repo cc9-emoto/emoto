9617d4fd0ee0ab9a34dca99798101a1d08891f9c
Wilson Lau <hello@wilsonplau.com>
2019-09-09 16:44:50 +0900

    Updated index page with styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import spotifyLogo from '../assets/images/spotifyLogo.png'
 
 const Login = () => {
   const handleLogin = async () => {
@@ -8,8 +9,11 @@ const Login = () => {
   };
 
   return (
-    <div className="signup">
-      <button onClick={handleLogin}>Login with Spotify</button>
+    <div className="login">
+      <button onClick={handleLogin}>
+        <img src={spotifyLogo} />
+        Login with Spotify
+      </button>
     </div>
   );
 };
